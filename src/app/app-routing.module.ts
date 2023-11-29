54f7714c887fad150c5d2544b359408443c6360c
DESKTOP-RNK2HL9\DELL <dthiep.gm@gmail.com>
2023-11-30 00:56:21 +0700

    section11/154-passing-static-data-to-a-route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { EditServerComponent } from "./servers/edit-server/edit-server.component
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
@@ -32,9 +33,14 @@ const appRoutes: Routes = [
       },
     ],
   },
+  // {
+  //   path: "not-found",
+  //   component: PageNotFoundComponent,
+  // },
   {
     path: "not-found",
-    component: PageNotFoundComponent,
+    component: ErrorPageComponent,
+    data: { message: "page not found!" },
   },
   {
     path: "**",
